dafa0fe158c7a4cf7aa16523f49bb19dc7dc083b
Abuzar <abuzarkhan1242@gmail.com>
2025-04-20 17:21:31 +0500

    working in entities

---
+++ src/main/java/com/abuzar/JavaWithDB/JavaWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.abuzar.JavaWithDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWithDbApplication.class, args);
+	}
+
+}
